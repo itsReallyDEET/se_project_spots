e415fad55e04a2f53362e26e40d233448c710819
Marcus Jr <marcusrrob@gmail.com>
2026-02-12 22:38:47 -0800

    Spots-Final-Stage

--- src/scripts/validation.js
+++ src/scripts/validation.js
@@ -1,4 +1,4 @@
-const settings = {
+export const settings = {
   formSelector: ".modal__form",
   inputSelector: ".modal__input",
   submitButtonSelector: ".modal__submit-btn",
@@ -41,12 +41,12 @@ const toggleButtonState = (inputList, buttonEl, config) => {
   }
 };
 
-const disableButton = (buttonEl, config) => {
+export const disableButton = (buttonEl, config) => {
   buttonEl.disabled = true;
   buttonEl.classList.add(config.inactiveButtonClass);
 };
 
-const resetValidation = (formEl, inputList, config) => {
+export const resetValidation = (formEl, inputList, config) => {
   inputList.forEach((inputEl) => {
     hideInputError(formEl, inputEl, config);
   });
@@ -66,11 +66,13 @@ const setEventListeners = (formEl, config) => {
   });
 };
 
-const enableValidation = (config) => {
+export const enableValidation = (config) => {
   const formList = document.querySelectorAll(config.formSelector);
   formList.forEach((formEl) => {
+    if (!formEl.querySelector(config.inputSelector)) {
+      return;
+    }
+
     setEventListeners(formEl, config);
   });
 };
-
-enableValidation(settings);
